37b5b4dd87e2c7c3b2fea3181b582013f8aa50ea
Ivan Zuev <ivan.zuev96@yandex.ru>
2022-10-31 20:55:57 +0300

    Update

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,7 +1,7 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int CalcSqr (int Bottom, int Top) {
+    public int CalcSqr(int Bottom, int Top) {
         int iteration = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= Bottom) {
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,8 +1,11 @@
 package ru.netology.sqr;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class SQRServiceTest {
@@ -11,11 +14,6 @@ public class SQRServiceTest {
     @CsvFileSource(files="src/test/resources/squarerange.csv")
     public void squared(int expected, int Bottom, int Top) {
         SQRService service = new SQRService();
-
-       // 3, 200, 300, positive test
-       // 5, 200, 300, Negative test
-       // 8, 200, 500, positive test
-       // 9, 200, 500, Negative test
 
 
         int actual = service.CalcSqr(Bottom, Top);
